ed2351726a8db2940e8e97173b05fa9e0e21746b
kannachan07 <93027997+kannachan07@users.noreply.github.com>
2021-12-25 05:23:58 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -31,18 +31,15 @@ def stats(update, context):
     cpuUsage = psutil.cpu_percent(interval=0.5)
     memory = psutil.virtual_memory().percent
     disk = psutil.disk_usage('/').percent
-    stats = f'<b>▶ Rᴜɴɴɪɴɢ Sɪɴᴄᴇ ▶ :</b> <code>{currentTime}</code>\n' \
-            f'<b>💾 Total Disk Space :</b> <code>{total}</code>\n' \
-            f'<b>📀 Used :</b> <code>{used}</code> ' \
-            f'<b>💿 Free :</b> <code>{free}</code>\n\n' \
-            f'<b>🔼 Upload :</b> <code>{sent}</code>\n' \
-            f'<b>🔽 Download :</b> <code>{recv}</code>\n\n' \
-            f'<b>🖥️ CPU :</b> <code>{cpuUsage}%</code> ' \
-            f'<b>🎮 RAM :</b> <code>{memory}%</code> ' \
-            f'<b>💽 DISK :</b> <code>{disk}%</code>' \
-            f'<b> </b>\n' \
-            f'<b>《 ☣️ @kannachan_mirrordrive ☣️ 》</b>'
-            
+    stats = f'<b>Bot Uptime:</b> <code>{currentTime}</code>\n' \
+            f'<b>Total Disk Space:</b> <code>{total}</code>\n' \
+            f'<b>Used:</b> <code>{used}</code> ' \
+            f'<b>Free:</b> <code>{free}</code>\n\n' \
+            f'<b>Upload:</b> <code>{sent}</code>\n' \
+            f'<b>Download:</b> <code>{recv}</code>\n\n' \
+            f'<b>CPU:</b> <code>{cpuUsage}%</code> ' \
+            f'<b>RAM:</b> <code>{memory}%</code> ' \
+            f'<b>DISK:</b> <code>{disk}%</code>'
     sendMessage(stats, context.bot, update)
 
 
